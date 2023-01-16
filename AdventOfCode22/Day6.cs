7f3303bad96e122716c435eb478109a3e9ca4961
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-01-16 19:32:15 +0100

    day 6, v1

---
+++ AdventOfCode22/Day6.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode22;
+
+public class Day6
+{
+    [Fact]
+    public void Part1()
+    {
+        var input = File.ReadAllText("day6.txt");
+
+        var result = GetStartIndex(input, 4);
+
+        Assert.Equal(1542, result);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        var input = File.ReadAllText("day6.txt");
+
+        var result = GetStartIndex(input, 14);
+
+        Assert.Equal(3153, result);
+    }
+
+    static int GetStartIndex(string input, int distinctChars)
+    {
+        HashSet<char> set = new();
+        for (int i = distinctChars; i < input.Length; i++)
+        {
+            set.Clear();
+            for (int j = 0; j < distinctChars; j++)
+            {
+                set.Add(input[i - 1 - j]);
+            }
+
+            if (set.Count == distinctChars)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
