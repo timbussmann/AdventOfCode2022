6bbb559e32a94d4cc05313bdafdc86e3f8ed6f78
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-01-16 19:55:37 +0100

    use different approaches for part 1 and 2

--- AdventOfCode22/Day6.cs
+++ AdventOfCode22/Day6.cs
@@ -5,6 +5,27 @@ public class Day6
     [Fact]
     public void Part1()
     {
+        int GetStartIndex(string input, int distinctChars)
+        {
+            // requires less allocations as we can reuse the set
+            HashSet<char> set = new();
+            for (int i = distinctChars; i < input.Length; i++)
+            {
+                set.Clear();
+                for (int j = 0; j < distinctChars; j++)
+                {
+                    set.Add(input[i - 1 - j]);
+                }
+
+                if (set.Count == distinctChars)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
         var input = File.ReadAllText("day6.txt");
 
         var result = GetStartIndex(input, 4);
@@ -15,30 +36,23 @@ public class Day6
     [Fact]
     public void Part2()
     {
+        int GetStartIndex(string input, int distinctChars)
+        {
+            for (int i = distinctChars; i < input.Length; i++)
+            {
+                if (input.Substring(i - distinctChars, distinctChars).Distinct().Count() == distinctChars)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
         var input = File.ReadAllText("day6.txt");
 
         var result = GetStartIndex(input, 14);
 
         Assert.Equal(3153, result);
-    }
-
-    static int GetStartIndex(string input, int distinctChars)
-    {
-        HashSet<char> set = new();
-        for (int i = distinctChars; i < input.Length; i++)
-        {
-            set.Clear();
-            for (int j = 0; j < distinctChars; j++)
-            {
-                set.Add(input[i - 1 - j]);
-            }
-
-            if (set.Count == distinctChars)
-            {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
