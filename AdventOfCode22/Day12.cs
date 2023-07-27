09310206308514ea5aec0226913abf98a2ded037
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-07-27 21:59:00 +0200

    part 2

--- AdventOfCode22/Day12.cs
+++ AdventOfCode22/Day12.cs
@@ -5,23 +5,47 @@ public class Day12
     [Fact]
     public async void Part1()
     {
-        int[][] grid = await ParseGrid();
+        var input = await File.ReadAllLinesAsync("day12.txt");
+        var grid = Grid.Parse(input);
 
-        var result =  FindShortestPath(grid);
+        var result =  FindShortestPath(grid, grid.Start);
         
         Assert.Equal(408, result);
     }
-
-    int FindShortestPath(int[][] levelMap)
+    
+    [Fact]
+    public async void Part2()
     {
-        int[][] distanceMap = Enumerable.Range(0, levelMap.Length)
+        var input = await File.ReadAllLinesAsync("day12.txt");
+        var grid = Grid.Parse(input);
+
+        var results = new List<int>();
+        for (int y = 0; y < grid.Count; y++)
+        {
+            for (int x = 0; x < grid[0].Length; x++)
+            {
+                if (grid[y][x] == 0)
+                {
+                    results.Add(FindShortestPath(grid, new Pos(y, x)));
+                }
+            }
+        }
+
+        var result = results.Min();
+        
+        Assert.Equal(29, result);
+    }
+
+    int FindShortestPath(Grid levelMap, Pos startPosition)
+    {
+        int[][] distanceMap = Enumerable.Range(0, levelMap.Count)
             .Select(_ => Enumerable.Repeat(int.MaxValue, levelMap[0].Length).ToArray()).ToArray();
         distanceMap[0][0] = 0;
 
         var visitedNodes = new HashSet<Pos>();
         var nextNodes = new List<(int distance, Pos node)>()
         {
-            (0, FindStartPos(levelMap))
+            (0, startPosition)
         };
 
         while (nextNodes.Any())
@@ -32,7 +56,7 @@ public class Day12
             var currentLevel = Math.Max(0, levelMap[currentPos.y][currentPos.x]);
             nextNodes.Remove(currentNode);
 
-            foreach (var neighbor in GetNeighbors(currentPos, levelMap.Length - 1, levelMap[0].Length - 1))
+            foreach (var neighbor in GetNeighbors(currentPos, levelMap.Count - 1, levelMap[0].Length - 1))
             {
                 //check if not visited already
                 if (visitedNodes.Contains(neighbor))
@@ -41,14 +65,15 @@ public class Day12
                 }
                 
                 var neighborLevel = levelMap[neighbor.y][neighbor.x];
-                if (neighborLevel == 1000 && currentLevel >= ('y' - 'a'))
-                {
-                    // no need to complete other paths since all node distances are equal
-                    return currentDistance + 1;
-                }
-
+                // check if neighbor is reachable
                 if (neighborLevel <= currentLevel + 1)
                 {
+                    if (neighbor == levelMap.Goal)
+                    {
+                        // no need to complete other paths since all node distances are equal
+                        return currentDistance + 1;
+                    }
+                    
                     // node reachable
                     var distanceToNode = currentDistance + 1;
                     if (distanceToNode < distanceMap[neighbor.y][neighbor.x])
@@ -66,7 +91,7 @@ public class Day12
             nextNodes.RemoveAll(tuple => tuple.node == currentPos);
         }
 
-        throw new Exception("no path found");
+        return int.MaxValue;
     }
 
     readonly record struct Pos(int y, int x);
@@ -94,45 +119,36 @@ public class Day12
         }
     }
 
-    async Task<int[][]> ParseGrid()
+    class Grid : List<int[]>
     {
-        var lines = (await File.ReadAllLinesAsync("day12.txt")).ToArray();
-
-        var grid = new int[lines.Length][];
-        for (int i = 0; i < grid.Length; i++)
+        public static Grid Parse(string[] input)
         {
-            grid[i] = lines[i].Select(c =>
+            var grid = new Grid();
+            for (int y = 0; y < input.Length; y++)
             {
-                if (c == 'S')
+                grid.Add(input[y].Select((c, x) =>
                 {
-                    return -1000;
-                }
+                    if (c == 'S')
+                    {
+                        grid.Start = new Pos(y, x);
+                        return 0;
+                    }
 
-                if (c == 'E')
-                {
-                    return 1000;
-                }
+                    if (c == 'E')
+                    {
+                        grid.Goal = new Pos(y, x);
+                        return 'z' - 'a';
+                    }
                 
-                return c - 'a';
-            }).ToArray();
-        }
-
-        return grid;
-    }
-
-    Pos FindStartPos(int[][] heightMap)
-    {
-        for (int y = 0; y < heightMap.Length; y++)
-        {
-            for (int x = 0; x < heightMap[0].Length; x++)
-            {
-                if (heightMap[y][x] == -1000)
-                {
-                    return new Pos(y, x);
-                }
+                    return c - 'a';
+                }).ToArray());
             }
+
+            return grid;
         }
 
-        throw new ArgumentException("No start position on map");
+        public Pos Goal { get; set; }
+
+        public Pos Start { get; set; }
     }
 }
