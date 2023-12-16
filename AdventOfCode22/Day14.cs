10c23ac331bc7ca90294ad795021e0823b3e3f98
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-12-16 15:05:10 +0100

    some refactorings

--- AdventOfCode22/Day14.cs
+++ AdventOfCode22/Day14.cs
@@ -16,68 +16,49 @@ public class Day14
         var input = await File.ReadAllLinesAsync("day14.txt");
         var lines = ParseLines(input);
         
-        var map = new int[1000,1000];
+        var maxY = FindMaximumY(lines);
 
-        int globalYMax = 0;
-        foreach (var line in lines)
-        {
-            foreach (var edge in line)
-            {
-                var xMin = Math.Min(edge.First.Item1, edge.Second.Item1);
-                var xMax = Math.Max(edge.First.Item1, edge.Second.Item1);
-                var yMin = Math.Min(edge.First.Item2, edge.Second.Item2);
-                var yMax = Math.Max(edge.First.Item2, edge.Second.Item2);
-                globalYMax = Math.Max(globalYMax, yMax);
-
-                for (int i = xMin; i <= xMax; i++)
-                {
-                    map[yMin, i] = BlockValue;
-                }
-
-                for (int i = yMin; i < yMax; i++)
-                {
-                    map[i, xMin] = BlockValue;
-                }
-            }
-        }
+        // +1 because of zero index and +1 for the fall-through check
+        var map = new int[maxY + 2, 1000];
+        AddLines(lines, map);
 
         int counter = 0;
-        Point start = (500, 0);
+        Point currentPosition = (500, 0);
         do
         {
-            if (map[start.y + 1, start.x] <= 0)
+            if (map[currentPosition.y + 1, currentPosition.x] <= 0)
             {
-                start.y++;
+                currentPosition.y++;
             }
             else
             {
                 // found block or other sand
                 // try left
-                if (map[start.y + 1, start.x - 1] <= 0)
+                if (map[currentPosition.y + 1, currentPosition.x - 1] <= 0)
                 {
-                    start.y++;
-                    start.x--;
+                    currentPosition.y++;
+                    currentPosition.x--;
                 }
                 else
                 {
                     // left blocked
                     // try right
-                    if (map[start.y + 1, start.x + 1] <= 0)
+                    if (map[currentPosition.y + 1, currentPosition.x + 1] <= 0)
                     {
-                        start.y++;
-                        start.x++;
+                        currentPosition.y++;
+                        currentPosition.x++;
                     }
                     else
                     {
                         // right blocked too
                         // rest at current pos
-                        map[start.y, start.x] = 1;
-                        start = (500, 0); // new sand block
+                        map[currentPosition.y, currentPosition.x] = 1;
+                        currentPosition = (500, 0); // new sand block
                         counter++;
                     }
                 }
             }
-        } while (start.y <= globalYMax);
+        } while (currentPosition.y <= maxY);
         
         Assert.Equal(1003, counter);
     }
@@ -102,14 +83,76 @@ public class Day14
         var input = await File.ReadAllLinesAsync("day14.txt");
         var lines = ParseLines(input);
 
-        var allPoints = lines
-            .SelectMany(l => l
-                .SelectMany(p => new[] { p.First, p.Second }))
-            .ToArray();
-        var maxY = allPoints.Max(p => p.y);
-        
+        var maxY = FindMaximumY(lines);
+
         // +1 because of zero index and +2 due to part 2 addition
         var map = new int[maxY + 3, 1000];
+        AddLines(lines, map);
+
+        // add bottom line for part 2
+        for (int i = 0; i < map.GetLength(1); i++)
+        {
+            map[map.GetLength(0) - 1, i] = BlockValue;
+        }
+        
+        int counter = 0;
+        Point current = (500, 0);
+        do
+        {
+            var next = FindNextPosition(map, current, ref counter);
+            if (current == next)
+            {
+                map[current.y, current.x] = 1;
+                current = (500, 0); // new sand block
+                counter++;
+            }
+            else
+            {
+                current = next;
+            }
+
+        } while (map[0, 500] <= 0);
+        
+        Assert.Equal(25771, counter);
+    }
+
+    private static Point FindNextPosition(int[,] map, Point position, ref int counter)
+    {
+        if (map[position.y + 1, position.x] <= 0)
+        {
+            position.y++;
+        }
+        else
+        {
+            // found block or other sand
+            // try left
+            if (map[position.y + 1, position.x - 1] <= 0)
+            {
+                position.y++;
+                position.x--;
+            }
+            else
+            {
+                // left blocked
+                // try right
+                if (map[position.y + 1, position.x + 1] <= 0)
+                {
+                    position.y++;
+                    position.x++;
+                }
+                else
+                {
+                    // right blocked too
+                    // rest at current pos
+                }
+            }
+        }
+
+        return position;
+    }
+
+    private static void AddLines(IEnumerable<IEnumerable<(Point First, Point Second)>> lines, int[,] map)
+    {
         foreach (var line in lines)
         {
             foreach (var edge in line)
@@ -130,50 +173,15 @@ public class Day14
                 }
             }
         }
+    }
 
-        for (int i = 0; i < map.GetLength(1); i++)
-        {
-            map[map.GetLength(0) - 1, i] = BlockValue;
-        }
-        
-        int counter = 0;
-        Point start = (500, 0);
-        do
-        {
-            if (map[start.y + 1, start.x] <= 0)
-            {
-                start.y++;
-            }
-            else
-            {
-                // found block or other sand
-                // try left
-                if (map[start.y + 1, start.x - 1] <= 0)
-                {
-                    start.y++;
-                    start.x--;
-                }
-                else
-                {
-                    // left blocked
-                    // try right
-                    if (map[start.y + 1, start.x + 1] <= 0)
-                    {
-                        start.y++;
-                        start.x++;
-                    }
-                    else
-                    {
-                        // right blocked too
-                        // rest at current pos
-                        map[start.y, start.x] = 1;
-                        start = (500, 0); // new sand block
-                        counter++;
-                    }
-                }
-            }
-        } while (map[0, 500] <= 0);
-        
-        Assert.Equal(25771, counter);
+    private static int FindMaximumY(IEnumerable<IEnumerable<(Point First, Point Second)>> lines)
+    {
+        var allPoints = lines
+            .SelectMany(l => l
+                .SelectMany(p => new[] { p.First, p.Second }))
+            .ToArray();
+        var maxY = allPoints.Max(p => p.y);
+        return maxY;
     }
 }
