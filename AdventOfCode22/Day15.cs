9e6e5e4959f25cbf04d8f0609ad999d0e9b7f37a
Tim Bussmann <timbussmann@users.noreply.github.com>
2024-01-07 00:15:49 +0100

    basic implementation for part 2, working with sample data

--- AdventOfCode22/Day15.cs
+++ AdventOfCode22/Day15.cs
@@ -24,15 +24,56 @@ public class Day15
             return new Reading(sensor, beacon);
         }).ToArray();
 
-        var rowArray = new CoordinateSystem(readings.Min(r => r.MinX), readings.Max(r => r.MaxX) + 1, row, row);
+        var coordinateSystem = new CoordinateSystem(readings.Min(r => r.MinX), readings.Max(r => r.MaxX) + 1, row, row);
         foreach (var reading in readings)
         {
-            reading.AddToCoordinateSystem(row, rowArray);
+            reading.AddToCoordinateSystem(row, coordinateSystem);
         }
         
-        var total = rowArray.GetRow(row).Where(x => x == 1).Sum();
+        var total = coordinateSystem.GetRow(row).Where(x => x == 1).Sum();
         
         Assert.Equal(expectedResult, total);
+    }
+
+    [Theory]
+    [InlineData("day15.test.txt", 20, 56000011)]
+    //[InlineData("day15.txt", 4000000, 56000011)] //requires too much memory
+    public async Task Part2(string fileName, int max, int expectedResult)
+    {
+        var input = await File.ReadAllLinesAsync(fileName);
+        var regex = new Regex("x=(-?\\d+), y=(-?\\d+)");
+        
+        var readings = input.Select(line =>
+        {
+            var matches = regex.Matches(line);
+            var sensor = new Coordinate(int.Parse(matches[0].Groups[1].ValueSpan), int.Parse(matches[0].Groups[2].ValueSpan));
+            var beacon = new Coordinate(int.Parse(matches[1].Groups[1].ValueSpan), int.Parse(matches[1].Groups[2].ValueSpan));
+            return new Reading(sensor, beacon);
+        }).ToArray();
+        
+        var coordinateSystem = new CoordinateSystem(0, max + 1, 0, max + 1, false);
+        foreach (var reading in readings)
+        {
+            for (int i = 0; i <= max; i++)
+            {
+                reading.AddToCoordinateSystem(i, coordinateSystem);
+            }
+        }
+
+        int counter = 0;
+        Coordinate signal = default;
+        for (int y = 0; y <= max; y++)
+        {
+            var index = coordinateSystem.GetRow(y).ToList().FindIndex(v => v == 0);
+            if (index >= 0)
+            {
+                counter++;
+                signal = new Coordinate(index, y);
+            }
+        }
+
+        var frequency = (signal.X * 4000000) + signal.Y; 
+        Assert.Equal(expectedResult, frequency);
     }
     
     public class Reading
@@ -97,8 +138,11 @@ public class CoordinateSystem
     int[][] array;
     readonly int xMin;
     readonly int yMin;
+    readonly bool throwOnOutOfBoundAccess;
+    readonly int xMax;
+    readonly int yMax;
 
-    public CoordinateSystem(int xMin, int xMax, int yMin, int yMax)
+    public CoordinateSystem(int xMin, int xMax, int yMin, int yMax, bool throwOnOutOfBoundAccess = true)
     {
         if (xMax < 0 || yMax < 0)
         {
@@ -109,32 +153,46 @@ public class CoordinateSystem
         var yWidth = Math.Abs(yMax - yMin) + 1;
         this.xMin = xMin;
         this.yMin = yMin;
-        
+        this.xMax = xMax;
+        this.yMax = yMax;
+        this.throwOnOutOfBoundAccess = throwOnOutOfBoundAccess;
+
         array = new int[yWidth][];
         for(int i = 0; i < yWidth; i++)
         {
             array[i] = new int[xWidth];
         }
     }
+
+    bool IsWithinBoundary(int x, int y)
+    {
+        if (throwOnOutOfBoundAccess)
+        {
+            return true; // array access itself will throw
+        }
+
+        return x >= xMin && x <= xMax && y >= yMin && y <= yMax;
+    }
     
     public int this[int x, int y]
     {
         get
         {
-            //TODO constraint checks
             return array[Math.Abs(yMin - y)][Math.Abs(xMin - x)];
         }
 
         set
         {
-            //TODO constraint checks
-            array[Math.Abs(yMin - y)][Math.Abs(xMin - x)] = value;
+            if (IsWithinBoundary(x, y))
+            {
+                array[Math.Abs(yMin - y)][Math.Abs(xMin - x)] = value;
+            }
         }
     }
 
     public void TrySet(int x, int y, int value)
     {
-        try
+        if (IsWithinBoundary(x, y))
         {
             var y_ = Math.Abs(yMin - y);
             var x_ = Math.Abs(xMin - x);
@@ -142,11 +200,6 @@ public class CoordinateSystem
             {
                 array[y_][x_] = value;
             }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
         }
     }
 
